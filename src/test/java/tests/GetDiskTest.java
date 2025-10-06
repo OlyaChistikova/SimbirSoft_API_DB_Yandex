d84c980812dfe72d3a7936321130043cfc44dd6d
Olya Chistikova <olya.chistikova@mail.ru>
2025-10-06 14:44:12 +0300

    Added method for GET without auth in GetDiskTest

--- src/test/java/tests/GetDiskTest.java
+++ src/test/java/tests/GetDiskTest.java
@@ -1,12 +1,12 @@
 package tests;
 
-import helpers.BaseRequests;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static helpers.BaseRequests.DISK_PATH;
-import static helpers.BaseRequests.LOGIN;
+import java.util.Map;
+
+import static helpers.BaseRequests.*;
 
 public class GetDiskTest extends BaseTest{
 
@@ -20,10 +20,17 @@ public class GetDiskTest extends BaseTest{
 
     @Test
     public void getDiskWithoutAuthTest(){
-        Response responseGetDisk = BaseRequests.sendGetRequestWithoutAuth(DISK_PATH, null, 401);
+        Response responseGetDisk = sendGetRequestWithoutAuth(DISK_PATH, null, 401);
 
         Assert.assertEquals(responseGetDisk.path("error"), "UnauthorizedError");
         Assert.assertNotNull(responseGetDisk.path("description"));
         Assert.assertNotNull(responseGetDisk.path("message"));
+    }
+
+    /**
+     * Отправляет GET-запрос без авторизации и проверяет статус.
+     */
+    public Response sendGetRequestWithoutAuth(String endpoint, Map<String, String> params, int expectedStatus) {
+        return sendGetRequest(false, endpoint, params, expectedStatus);
     }
 }
