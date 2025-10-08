312337b9d34ac85e2bbe0c985a7399315777a9e7
Olya Chistikova <olya.chistikova@mail.ru>
2025-10-08 17:06:19 +0300

    Added new parameters for paths, credentials, and files, created methods for sending PUT request with body and without a base URL and POST request in BaseRequests

--- src/test/java/helpers/BaseRequests.java
+++ src/test/java/helpers/BaseRequests.java
@@ -39,7 +39,7 @@ public class BaseRequests {
     /**
      * Имя пользователя для авторизации.
      */
-    public static final String LOGIN = ParametersProvider.getProperty("username");
+    public static final String LOGIN = ParametersProvider.getProperty("login");
 
     /**
      * Путь для взаимодействия с диском.
@@ -47,7 +47,58 @@ public class BaseRequests {
     public static final String DISK_PATH = ParametersProvider.getProperty("disk_path");
 
     /**
+     * Путь для загрузки ресурсов
+     */
+    public static final String RESOURCES_UPLOAD = ParametersProvider.getProperty("resources_upload");
+
+    /**
+     * Путь для копирования ресурсов.
+     */
+    public static final String RESOURCES_COPY = ParametersProvider.getProperty("resources_copy");
+
+    /**
+     * Путь для скачивания ресурсов.
+     */
+    public static final String RESOURCES_DOWNLOAD = ParametersProvider.getProperty("resources_download");
+
+    /**
+     * Папка для входных файлов.
+     */
+    public static final String INPUT_FOLDER = ParametersProvider.getProperty("inputFolder");
+
+    /**
+     * Папка для выходных файлов.
+     */
+    public static final String OUTPUT_FOLDER = ParametersProvider.getProperty("outputFolder");
+
+    /**
+     * Папка SDET.
+     */
+    public static final String SDET_FOLDER = ParametersProvider.getProperty("sdetFolder");
+
+    /**
+     * Имя файла для теста.
+     */
+    public static final String FILE_NAME = ParametersProvider.getProperty("fileName");
+
+    /**
+     * Имя пользователя для теста.
+     */
+    public static final String USER_NAME = ParametersProvider.getProperty("username");
+
+    /**
+     * Пароль пользователя для теста.
+     */
+    public static final String PASSWORD = ParametersProvider.getProperty("password");
+
+    /**
+     * Путь к файлу для скачивания.
+     */
+    public static final String OUTPUT_FILE_PATH = ParametersProvider.getProperty("outputFilePath");
+
+    /**
      * Создает конфигурацию запроса с авторизацией.
+     *
      * @return объект RequestSpecification с настройками для авторизованного запроса.
      */
     private static RequestSpecification getRequestSpecWithAuth() {
@@ -60,6 +111,7 @@ public class BaseRequests {
 
     /**
      * Создает конфигурацию запроса без авторизации.
+     *
      * @return объект RequestSpecification с настройками для неавторизованного запроса.
      */
     private static RequestSpecification getRequestSpecWithoutAuth() {
@@ -70,10 +122,23 @@ public class BaseRequests {
     }
 
     /**
+     * Создает конфигурацию запроса без базового URL, только с содержимым и авторизацией.
+     *
+     * @return объект RequestSpecification без базового URL.
+     */
+    private static RequestSpecification getRequestSpecWithoutBaseUrl() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "OAuth " + TOKEN)
+                .build();
+    }
+
+    /**
      * Отправляет GET-запрос к API.
-     * @param withAuth указывает, нужно ли использовать авторизацию.
-     * @param endpoint относительный путь API.
-     * @param params параметры запроса.
+     *
+     * @param withAuth       указывает, нужно ли использовать авторизацию.
+     * @param endpoint       относительный путь API.
+     * @param params         параметры запроса.
      * @param expectedStatus ожидаемый HTTP статус ответа.
      * @return объект Response с ответом сервера.
      */
@@ -90,9 +155,10 @@ public class BaseRequests {
 
     /**
      * Отправляет PUT-запрос к API.
-     * @param withAuth указывает, нужно ли использовать авторизацию.
-     * @param endpoint относительный путь API.
-     * @param params параметры запроса.
+     *
+     * @param withAuth       указывает, нужно ли использовать авторизацию.
+     * @param endpoint       относительный путь API.
+     * @param params         параметры запроса.
      * @param expectedStatus ожидаемый HTTP статус ответа.
      * @return объект Response с ответом сервера.
      */
@@ -108,10 +174,29 @@ public class BaseRequests {
     }
 
     /**
+     * Отправляет PUT-запрос с телом без авторизации и без базового URL.
+     *
+     * @param endpoint       относительный путь API.
+     * @param expectedStatus ожидаемый HTTP статус.
+     * @param bodyData       содержимое тела запроса.
+     */
+    public static void sendPutRequestWithBody(String endpoint, int expectedStatus, String bodyData) {
+        given()
+                .spec(getRequestSpecWithoutBaseUrl())
+                .body(bodyData)
+                .when()
+                .put(endpoint)
+                .then()
+                .statusCode(expectedStatus)
+                .extract().response();
+    }
+
+    /**
      * Отправляет DELETE-запрос к API.
-     * @param withAuth указывает, нужно ли использовать авторизацию.
-     * @param endpoint относительный путь API.
-     * @param params параметры запроса.
+     *
+     * @param withAuth       указывает, нужно ли использовать авторизацию.
+     * @param endpoint       относительный путь API.
+     * @param params         параметры запроса.
      * @param expectedStatus ожидаемый HTTP статус ответа.
      * @return объект Response с ответом сервера.
      */
@@ -121,6 +206,26 @@ public class BaseRequests {
                 .params(params != null ? params : Map.of())
                 .when()
                 .delete(endpoint)
+                .then()
+                .statusCode(expectedStatus)
+                .extract().response();
+    }
+
+    /**
+     * Отправляет POST-запрос к API.
+     *
+     * @param withAuth       указывает, нужно ли использовать авторизацию.
+     * @param endpoint       относительный путь API.
+     * @param params         параметры запроса.
+     * @param expectedStatus ожидаемый HTTP статус ответа.
+     * @return объект Response с ответом сервера.
+     */
+    public static Response sendPostRequest(boolean withAuth, String endpoint, Map<String, String> params, int expectedStatus) {
+        return given()
+                .spec(withAuth ? getRequestSpecWithAuth() : getRequestSpecWithoutAuth())
+                .queryParams(params != null ? params : Map.of())
+                .when()
+                .post(endpoint)
                 .then()
                 .statusCode(expectedStatus)
                 .extract().response();
